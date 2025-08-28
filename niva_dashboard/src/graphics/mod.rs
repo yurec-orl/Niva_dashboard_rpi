8bd60e1fd260286bd6b696d5d6f7ff655e133262
yurec-orl <yurec.orl@gmail.com>
2025-08-28 21:44:18 +0100

    Code cleanup

--- niva_dashboard/src/graphics/mod.rs
+++ niva_dashboard/src/graphics/mod.rs
@@ -2,6 +2,3 @@ pub mod opengl_test;
 pub mod context;
 pub mod sdl2_gauges;
 
-pub use opengl_test::{run_opengl_test, run_dashboard_gauges_test, run_moving_needle_test, run_text_rendering_test, run_opengl_rotating_needles_demo, run_opengl_text_rendering_test};
-pub use context::GraphicsContext;
-pub use sdl2_gauges::{run_sdl2_gauges_test, run_sdl2_advanced_needles_test};
