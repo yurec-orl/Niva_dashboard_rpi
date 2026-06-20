6a5a765c668ed4f1a752e384bbf712275f1a8d1f
Yurii Orlov <yurec.orl@gmail.com>
2026-06-20 19:01:04 +0300

    Improve code

--- niva_dashboard/src/util/adc_data_provider.rs
+++ niva_dashboard/src/util/adc_data_provider.rs
@@ -1,12 +1,29 @@
-use crate::util::adc_serial_reader::{ADCSerialReader, SerialReader};
-use crate::hardware::hw_providers::*;
+use crate::util::adc_serial_reader::ADCSerialReader;
 
+use std::fmt;
 use std::thread;
 use std::sync::Arc;
-use std::sync::Mutex;
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Mutex;
 
 const MAX_DATA: usize = 32;           // Headroom if consumer lags
+
+/// Errors that can occur when starting the ADC data provider.
+#[derive(Debug)]
+pub enum AdcDataProviderError {
+    /// The provider was already started (cannot be run twice).
+    AlreadyStarted,
+}
+
+impl fmt::Display for AdcDataProviderError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::AlreadyStarted => write!(f, "ADC data provider already started"),
+        }
+    }
+}
+
+impl std::error::Error for AdcDataProviderError {}
 
 // Reads data from ADC module via USB serial in separate thread
 pub struct ADCDataProvider {
@@ -26,28 +43,41 @@ impl ADCDataProvider {
         }
     }
 
-    pub fn run(&mut self) {
-        let mut adc_reader = self.adc_reader.take().expect("already started");
+    /// Starts the background thread to read ADC data.
+    ///
+    /// Returns `Err` if the provider has already been started.
+    pub fn run(&mut self) -> Result<(), AdcDataProviderError> {
+        if self.adc_reader.is_none() {
+            return Err(AdcDataProviderError::AlreadyStarted);
+        }
+
+        let mut adc_reader = self.adc_reader.take().expect("checked above");
         let should_stop = Arc::clone(&self.should_stop);
         let data = Arc::clone(&self.data);
 
-        self.thread = Some(std::thread::spawn(move || {
-            while !should_stop.load(Ordering::Relaxed) {
-                let sample = adc_reader.read_line();
+        self.thread = Some(std::thread::Builder::new()
+            .name("adc-data-provider".into())
+            .spawn(move || {
+                while !should_stop.load(Ordering::Relaxed) {
+                    let sample = adc_reader.read_line();
 
-                let mut _data = data.lock().unwrap();
+                    let mut _data = data.lock().unwrap();
 
-                if _data.len() < MAX_DATA {
-                    if let Some(value) = sample {
-                        print!("DEBUG: {}\r\n", value);
-                        _data.push(value);
+                    if _data.len() < MAX_DATA {
+                        if let Some(value) = sample {
+                            print!("DEBUG: {}\r\n", value);
+                            _data.push(value);
+                        }
+                    } else {
+                        drop(_data); // Release lock before sleeping to avoid blocking consumer
+                        print!("WARNING: ADC data buffer full\r\n");
+                        thread::sleep(std::time::Duration::from_millis(10));
                     }
-                } else {
-                    print!("WARNING: ADC data buffer full\r\n");
-                    thread::sleep(std::time::Duration::from_millis(10));
                 }
-            }
-        }));
+            })?)
+        ;
+
+        Ok(())
     }    
 
     pub fn stop(&mut self) {
@@ -62,6 +92,8 @@ impl ADCDataProvider {
 impl Drop for ADCDataProvider {
     fn drop(&mut self) {
         self.stop();
-        self.thread.take().unwrap().join().expect("ADC data provider thread panicked");
+        if let Some(thread) = self.thread.take() {
+            let _ = thread.join(); // Ignore panic for clean shutdown
+        }
     }
 }
